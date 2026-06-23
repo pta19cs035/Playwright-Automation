7802e88f29b0c1645a99af84a8a19f5b173b674d
Prince <princemathew2044@gmail.com>
2026-06-23 13:36:59 +0530

    Skipped json params

--- tests/paramtest3json.spec.ts
+++ tests/paramtest3json.spec.ts
@@ -4,12 +4,12 @@ import { test, expect } from '@playwright/test';
 //Reading data from JSON
 const jsonPath="testdata/data.json";
 const loginData = JSON.parse(fs.readFileSync(jsonPath, "utf-8"));
-test.skip();
+
 test.describe('Login data driven test', async()=>{
 
 for(const {email, password, validity} of loginData)
 {
-test(`Login test for ${email} and ${password}`, async({page})=>{
+test.skip(`Login test for ${email} and ${password}`, async({page})=>{
 await page.goto('https://demowebshop.tricentis.com/login');
 // Fill login form
 await page.locator ('#Email').fill(email); await page.locator ('#Password').fill(password);
